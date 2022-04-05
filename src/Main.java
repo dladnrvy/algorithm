0df9753f10e091e813dcc935e63bbd4c2a12ea6f
dladnrvy <pyo0829@naver.com>
2022-04-05 11:48:34 +0900

    [알고리즘] 문자열수정 10. 문자열 압축

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,8 @@ import java.util.*;
 public class Main {
     public String solution(String s){
         String answer = "";
-        //[0]teachermode [1]e
-        String [] s_arr = s.split(" ");
-        //System.out.println(Arrays.toString(s_arr));
-        String str = s_arr[0];
-        char findC = s_arr[1].toCharArray()[0];
-        ArrayList<Integer> answerArr = new ArrayList<>();
-        //fkdgkjdflkgjljslgjkfldjlkfdg f
-        //0 1 2 3 3 2 1 0 1 2 3 4 5 6 5 4 3 2 1 0 1 2 3 2 1 0 1 2
-        //0 1 2 3 3 2 1 0 1 2 3 4 5 6 5 4 3 2 1 0 1 2 3 2 1 0 1 1
-        //eochjgoekghlakegh h
-        //3 2 1 0 1 2 3 3 2 1 0 1 2 3 2 1 0
 
-        int num = 0, chkNum = 0;
-        for(int i=0; i<str.length(); i++){
-            if( str.charAt(i) != findC) num++;
-            else num = 0;
-            answerArr.add(num);
-        }
 
-//1 2 1 0 1 2 3 3 2 1 0 1 2 3 2 1 0
-
-        for(int i=str.length()-1; i>=0; i--){
-            if( str.charAt(i) != findC) chkNum++;
-            else chkNum = 0;
-
-            //System.out.println(str.charAt(i)+" / "+ answerArr.get(i) + " / "+chkNum);
-            if( i == str.length() - 1 ) continue;
-            if(answerArr.get(i) > chkNum){
-                answerArr.set(i,chkNum);
-            }else if(  i == 0 ){
-                answerArr.set(i,chkNum);
-            }
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int item : answerArr) { stringBuilder.append(item+" "); }
-        answer = stringBuilder.toString();
         return answer;
     }
 
---
+++ src/Study_10.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Study_10 {
+// 알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
+// 경우 반복되는 문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로
+// 문자열을 압축하는 프로그램을 작성하시오.
+// 단 반복횟수가 1인 경우 생략합니다.
+
+    public String solution(String s) {
+        String answer = "";
+// 1.
+//        ArrayList<Character> arrStr = new ArrayList<>();
+//
+//        // KSTTTSEEKFKKKDJJGG
+//        // KST3SE2KFK3DJ2G2
+//        int cnt = 1;
+//        char[] c = s.toCharArray();
+//        for (int i=0; i<c.length; i++)
+//        {
+//            if( s.length() > i+1 ){
+//                if( c[i] == c[i+1]){
+//                    cnt++;
+//                }else{
+//                    answer += c[i];
+//
+//                    if(cnt != 1) {
+//                        answer += cnt;
+//                        cnt = 1;
+//                    }
+//                }
+//            }else{
+//                answer += c[i];
+//                if(cnt != 1){
+//                    answer += cnt;
+//                }
+//            }
+//        }
+//  2.
+        s = s+" ";
+        int cnt = 1;
+        // i는 빈문자 전까지만 체크
+        for(int i=0; i<s.length()-1; i++)
+        {
+            if(s.charAt(i) == s.charAt(i+1)) cnt ++;
+            else{
+                answer += s.charAt(i);
+                if(cnt > 1) answer += String.valueOf(cnt);
+                cnt=1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Study_10 T = new Study_10();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
